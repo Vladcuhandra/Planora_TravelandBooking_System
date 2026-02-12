2ebb1a6352292e6adc765214aee0f42b30b48e49
Kakha8 <kakha.kudava.98@gmail.com>
2026-02-12 17:00:52 +0200

    initial upload

---
+++ src/test/java/kakha/kudava/planora_travelandbooking_system/PlanoraTravelandBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package kakha.kudava.planora_travelandbooking_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanoraTravelandBookingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
