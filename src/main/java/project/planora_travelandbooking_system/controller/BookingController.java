a36714e6ccaf7608a08283f2fd375ad14cbac31b
seduav <sedu.venta@gmail.com>
2026-02-13 13:48:32 +0200

    Added controller classes

--- src/main/java/project/planora_travelandbooking_system/controller/BookingController.java
+++ src/main/java/project/planora_travelandbooking_system/controller/BookingController.java
@@ -1,10 +1,10 @@
-package project.planora_travelandbooking_system.controller;
+package project.planora_travelandbooking_system.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class BookingsController {
+public class BookingController {
 
     @GetMapping("/api/bookings")
     public String booking() {
