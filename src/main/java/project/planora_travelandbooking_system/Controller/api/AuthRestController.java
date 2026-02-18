efb79575fe195dc5cf6d764e032056e8d87bcb7a
Kakha8 <kakha.kudava.98@gmail.com>
2026-02-18 16:57:47 +0200

    jwt filter

--- src/main/java/project/planora_travelandbooking_system/Controller/api/AuthRestController.java
+++ src/main/java/project/planora_travelandbooking_system/Controller/api/AuthRestController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import project.planora_travelandbooking_system.Config.JwtUtil;
+import project.planora_travelandbooking_system.Security.JwtUtil;
 import project.planora_travelandbooking_system.DTO.UserDTO;
 
 import java.util.Map;
---
+++ src/main/java/project/planora_travelandbooking_system/Security/JwtFilter.java
@@ -0,0 +1,72 @@
+package project.planora_travelandbooking_system.Security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import project.planora_travelandbooking_system.Service.DbUserDetailService;
+
+import java.io.IOException;
+
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final JwtUtil jwtUtil;
+    private final DbUserDetailService userDetailsService;
+
+    public JwtFilter(JwtUtil jwtUtil,
+                                   DbUserDetailService userDetailsService) {
+        this.jwtUtil = jwtUtil;
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) {
+        String path = request.getRequestURI();
+        return path.equals("/api/auth/login") || path.equals("/api/user/restore");
+    }
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        String authHeader = request.getHeader("Authorization");
+
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            String token = authHeader.substring(7);
+
+            try {
+                String email = jwtUtil.extractEmail(token);
+
+                if (email != null &&
+                        SecurityContextHolder.getContext().getAuthentication() == null) {
+
+                    UserDetails userDetails =
+                            userDetailsService.loadUserByUsername(email);
+
+                    UsernamePasswordAuthenticationToken authentication =
+                            new UsernamePasswordAuthenticationToken(
+                                    userDetails,
+                                    null,
+                                    userDetails.getAuthorities()
+                            );
+
+                    authentication.setDetails(
+                            new WebAuthenticationDetailsSource().buildDetails(request)
+                    );
+
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                }
+
+            } catch (Exception e) {
+                // invalid token, will add logging later
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
